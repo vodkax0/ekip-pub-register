ecab0bf72c025eb20917a23cc21e90cb7c24a252
AnarchyX <100626938+AnarchyX0@users.noreply.github.com>
2022-03-22 18:36:27 +0300

    Update settings.js

--- src/configs/settings.js
+++ src/configs/settings.js
@@ -8,27 +8,27 @@
     //General Settings
     client.settings = {
 
-        "Prefix": ".",
-        "Token": "",
-        "Owners": [],
-        "VoiceChannel": "",
-        "Activity": "WATCHING",
-        "Status": "dnd",
-        "MongoURL": "",
-        "Footer": "SeonerVorteX",
-        "DisableCooldownsForAdmins": true,
+        "Prefix": ".",//prefix
+        "Token": "",//bot tokeni
+        "Owners": [],//bot kurucusu id
+        "VoiceChannel": "",//botun gireceği ses kanal id
+        "Activity": "WATCHING",//watching yaptık istiyorsanız PLAYING LİSTENİNG yapabilirsiniz
+        "Status": "dnd",//online/idle/dnd/offline
+        "MongoURL": "",//mongodb url'niz
+        "Footer": "Anarchy",//bot footeri
+        "DisableCooldownsForAdmins": true,//elleme
 
     };
 
     //Activity Messages
     client.statusMessages = [
 
-        "Only VorteX!",
-        "Created By VorteX!",
-        "discord.gg/MTNkXHnX3b"
+        "Anarchy",//1. durum
+        "Created By Anarchy",//2. durum
+        "Only Anarchy"//3. durum - botun hep durum değişmesini istemiyorsanız hepsini aynı yapın
 
     ];
-
+//emojiler kısmını elleme
     //Emojis
     client.systemEmojis = [
 
@@ -47,66 +47,66 @@
     client.guildSettings = {
 
         ///General
-        guildID: "",
-        guildTags: [], 
-        guildDiscriminator: "",
-        guildRulesChannel: "",
-        entryExitChannel: "",
-        guildChat: "",
-        tagLog: "",
-        botYt: "",
-        unAuthorizedMessages: true,
+        guildID: "",//sunucu id
+        guildTags: [], //sunucu tagı
+        guildDiscriminator: "",//sunucunun etiket tagı (yoksa boş bırak)
+        guildRulesChannel: "",//kurallar kanal id
+        entryExitChannel: "",//giren çıkan log kanal id
+        guildChat: "",//chat kanal id
+        tagLog: "",//tag log kanal id
+        botYt: "",//bir rol oluşturun sunucunuzdaki tüm botlara verin ve o rolün id sini buraya yapıştırın
+        unAuthorizedMessages: true,//elleme
 
         //Registration
         registration: {
 
-            staffs: [],
-            familyRole: "",
-            requireTag: false,
-            penalBlockLimit: 10,
-            penalPointBlockLimit: 150,
-            unifying: " | ",
-            nameTag: "•",
-            maxAge: 50,
-            minAge: 10,
-            limit: 15,
-            log: "",
+            staffs: [],//kayıt yetkili rol id
+            familyRole: "",//tag alanlara verilecek rol id
+            requireTag: false,//taglı alımdaysanız false yerine true yapın
+            penalBlockLimit: 10,//elleme
+            penalPointBlockLimit: 150,//elleme
+            unifying: " | ",//isim yaş arasına koyulacak işaret
+            nameTag: "tag",//isim tagınız
+            maxAge: 50,//maksimum yaş
+            minAge: 10,//minimum yaş
+            limit: 15,//kayıt limiti
+            log: "",//kayıt log
 
             //Man
             man: {
-                manRole: "",
-                otherManRoles: [],
+                manRole: "",//erkek rol id
+                otherManRoles: [],//diğer erkek rol id
             },
             
             //Woman
             woman: {
-                womanRole: "",
-                otherWomanRoles: [],
+                womanRole: "",//kadın rol id
+                otherWomanRoles: [],//diğer kadın rol id
             },
 
             //Vip
             vip: {
-                vipRole: "",
-                onlyAdmins: true,
-                dailyVipLimit: 5,
+                vipRole: "",//vip rol id
+                onlyAdmins: true,//false yaparsanız kayıt yetkilileri de vip verebilecek (sunucunuzun güvenli bir ortam olması için true kalsın)
+                dailyVipLimit: 5,//günlük verilecek vip sayısı
             },
 
         },
         
         //Unregister
         unregister: {
-            unregisterChannel: "",
-            unregisterRole: "",
-            unregisterName: "İsim | Yaş",
-            dailyUnregisterLimit: 10,
+            unregisterChannel: "",//sunucuya biri girince botun mesaj atacağı kanal id 
+            unregisterRole: "",//kayıtsız id
+            unregisterName: "İsim | Yaş",//sunucuya biri girince otomatik yapılacak isim
+            dailyUnregisterLimit: 10,//günlük kayıtsıza atma sayısı
         },
 
         //Quarantine
         quarantine: {
-            quarantineRole: "",
-            quarantineName: "Şüpheli Hesap",
-            quarantineLog: "",
-            quarantineDateLimit: 604800000,
+            quarantineRole: "",//şüpheli hesap rol id
+            quarantineName: "Şüpheli Hesap",//şüpheli hesaplara verilecek isim
+            quarantineLog: "",//şüpheli hesap log
+            quarantineDateLimit: 604800000,//elleme sikersin
         },
 
     };
